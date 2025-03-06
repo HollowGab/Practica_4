da07c73ea9e24a9e74d876c1ca7bf735da8bf713
HollowGab <gabri.areani411@gmail.com>
2025-03-06 14:04:39 -0600

    Update ejercicio1.c

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,1 +1,16 @@
+// Arellanes Aniceto Gabriel
+// ejercicio1
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    int a=3;
+    int b=2;
+    if(a>b)
+        {
+            printf(" a es mayor \n");
+        }
+    
+    return 0;
+}
